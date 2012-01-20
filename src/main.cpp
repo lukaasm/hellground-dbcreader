7ed45f82e9bf5b5bfd29c4da113b12c7b7431dbf
lukaasm <lukaasm@localhost>
2012-01-20 23:51:56 +0100

    back to no GUI version

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 /*
  */
 
-#include <FL/Fl.h>
-#include <FL/Fl_Window.h>
-#include <FL/Fl_Menu_Bar.h>
-
 #include "Defines.h"
 
 const char Spell_243_fmt[]="nixiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiifxiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiffffffiiiiiiiiiiiiiiiiiiiiifffiiiiiiiiiiiiiiifffixiixssssssssssssssssxssssssssssssssssxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxiiiiiiiiiixfffixxiiii";
@@ -18,13 +14,6 @@ const char* ext[] =
 
 int main(int argc, char* argv[])
 {
-    Fl_Window mainWindow(300, 200, "DBCReader by lukaasm");
-    mainWindow.begin();
-    {
-        Fl_Menu_Bar *menuBar = new Fl_Menu_Bar(0, 0, 640, 30);
-        menuBar->copy(menuitems);
-    }
-    mainWindow.end();
-    mainWindow.show();
-    return Fl::run();
+
+    return 0;
 }
